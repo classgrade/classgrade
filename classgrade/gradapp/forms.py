0ab9a315cedf21982d4eb312f892e7c0904a3d94
camille24 <camille.marini@gmail.com>
2016-09-22 01:29:49 +0200

    wip create assignment

--- classgrade/gradapp/forms.py
+++ classgrade/gradapp/forms.py
@@ -1,20 +1,9 @@
-from django import forms
+from django.forms import ModelForm
+from gradapp.models import Assignmentype
 
 
-class AssignmentypeForm(forms.Form):
-    title = forms.CharField(max_length=100,
-                            label='Assignment title',
-                            help_text='Max 100 char')
-    description = forms.CharField(max_length=500,
-                                  label='Assignment description (for students)',
-                                  help_text='Max 500 char',
-                                  widget=forms.Textarea)
-    nb_graders = forms.IntegerField(label='number of graders',
-                                    help_text='Default: 3')
-    file_type = forms.CharField(max_length=20, label='Assignment file type',
-                                help_text='Enter the file extension\
-                                          (default:ipynb)')
-    deadline_submission = forms.DateTimeField(label='Submission deadline',
-                                              help_text='%Y-%m-%d %H:%M')
-    deadline_grading = forms.DateTimeField(label='Grading deadline',
-                                           help_text='%Y-%m-%d %H:%M')
+class AssignmentypeForm(ModelForm):
+
+    class Meta:
+        model = Assignmentype
+        exclude = ['prof']
--- classgrade/gradapp/views.py
+++ classgrade/gradapp/views.py
@@ -1,7 +1,46 @@
+import csv
+import logging
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from gradapp.forms import AssignmentypeForm
 from gradapp.models import Assignmentype
+
+
+logger = logging.getLogger(__name__)
+
+
+def _make_error_message(e):
+    if hasattr(e, 'traceback'):
+        return str(e.traceback)
+    else:
+        return repr(e)
+
+
+def get_students(csv_file):
+    """
+    :param csv_file: csv file with list of students.\
+        Each row contains: first_name, last_name, email
+    :type csv_file: str
+    :rtype: 2 lists existing_students and new_students [[username, email], ..]
+    """
+
+    with open(csv_file) as ff:
+        reader = csv.reader(ff, delimiter=',')
+        existing_students = []
+        new_students = []
+        for i, row in enumerate(reader):
+            row = [x.strip() for x in row[:3]]
+            username = "_".join(row[:2])
+            username = username.replace(" ", "_")
+            email = row[2]
+            try:
+                u = User.objects.get(username=username)
+                existing_students.append([u.username, u.email])
+            except ObjectDoesNotExist:
+                new_students.append([username, email])
+    return existing_students, new_students
 
 
 def index(request):
@@ -11,28 +50,34 @@ def index(request):
 
 @login_required
 def create_assignmentype(request):
+    try:
+        prof = request.user.prof
+    except ObjectDoesNotExist:
+        return redirect('gradapp:index')
     context = {}
     if request.method == 'POST':
-        form = AssignmentypeForm(request.POST)
+        form = AssignmentypeForm(request.POST, request.FILES)
         if form.is_valid():
             if not Assignmentype.objects.filter(title=form.
                                                 cleaned_data['title']):
-                new_assignmentype =\
-                    Assignmentype(title=form.cleaned_data['title'],
-                                  description=form.cleaned_data['description'],
-                                  nb_graders=form.cleaned_data['nb_graders'],
-                                  file_type=form.cleaned_data['file_type'],
-                                  deadline_submission=form.
-                                  cleaned_data['deadline_submission'],
-                                  deadline_grading=form.
-                                  cleaned_data['deadline_grading'],
-                                  prof=request.user)
+                new_assignmentype = form.save(commit=False)
+                new_assignmentype.prof = prof
                 new_assignmentype.save()
                 # get list students from csv file
-
-                return redirect("gradapp:index")
+                try:
+                    existing_students, new_students =\
+                        get_students(new_assignmentype.list_students.path)
+                    # return page asking for agreement for creation of students
+                    return redirect("gradapp:index")
+                except Exception as e:
+                    logger.error(_make_error_message(e))
+                    new_assignmentype.list_students = None
+                    new_assignmentype.save()
+                    # return details page of assignmentype
+                    return redirect('/assignmentype/%s' %
+                                    new_assignmentype.pk)
             else:
-                context['error'] = 'Oups, this assignment title has, \
+                context['error'] = 'Oups, this assignment title has \
                                     already been used, change it!'
     else:
         form = AssignmentypeForm()

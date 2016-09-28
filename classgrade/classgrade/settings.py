76fa4c13bd0b0cebb89c1d342c661793446f6ead
camille24 <camille.marini@gmail.com>
2016-09-28 18:27:13 +0200

    added datetimepicker

--- classgrade/classgrade/settings.py
+++ classgrade/classgrade/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'markdownify',
+    'datetimewidget',
 ]
 
 MIDDLEWARE = [
--- classgrade/gradapp/forms.py
+++ classgrade/gradapp/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from gradapp.models import Assignmentype, Assignment, Evalassignment
+from datetimewidget.widgets import DateTimeWidget
 
 
 class AssignmentypeForm(ModelForm):
@@ -7,6 +8,12 @@ class AssignmentypeForm(ModelForm):
     class Meta:
         model = Assignmentype
         exclude = ['prof', 'archived']
+        widgets = {
+            'deadline_submission':
+            DateTimeWidget(usel10n=True, bootstrap_version=3),
+            'deadline_grading':
+            DateTimeWidget(usel10n=True, bootstrap_version=3),
+        }
 
 
 class LightAssignmentypeForm(ModelForm):
